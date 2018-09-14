0311f879813a535fec139e1a67105361e22fd7a5
Deepak Ghimire <deepak@keti.re.kr>
2018-09-14 16:54:52 +0900

    update

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ eye color detection
 2. [HTML Color Picker](https://www.w3schools.com/colors/colors_picker.asp?colorhex=ffff00)
 3. [What Is the HSV Color Model?](https://www.lifewire.com/what-is-hsv-in-design-1078068)
 4. [RGB to HSV color conversion](https://www.rapidtables.com/convert/color/rgb-to-hsv.html)
+5. [HSV Color Range](https://github.com/jeffreyolchovy/whatismyeyecolor/edit/master/library/src/main/scala/com/whatismyeyecolor/ColorRange.scala)
--- eye-color.py
+++ eye-color.py
@@ -7,22 +7,44 @@ import cv2
 import time
 import argparse
 
+# img1 l(200, 220) | r(305, 213)
+# img2 l(295, 440) | r(595, 420)
+# img3 l(128, 186) | r(260, 190)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--image_path', default='data/2.jpg')
-parser.add_argument('--lx', type=int, default=200)
-parser.add_argument('--ly', type=int, default=220)
-parser.add_argument('--rx', type=int, default=305)
-parser.add_argument('--ry', type=int, default=213)
+parser.add_argument('--lx', type=int, default=295)
+parser.add_argument('--ly', type=int, default=440)
+parser.add_argument('--rx', type=int, default=595)
+parser.add_argument('--ry', type=int, default=420)
 args = parser.parse_args()
 
-def eye_color(image1, lcenter, rcenter):
-    imgHSV = cv2.cvtColor(image1, cv2.COLOR_BGR2HLS)
+# define HSV color ranges for eyes color
+Blue        =   ((166, 21, 50), (240, 100, 85))
+BlueGray    =   ((166, 2, 25), (300, 20, 75))
+Brown       =   ((2, 20, 20), (40, 100, 60))
+BrownGray   =   ((20, 3, 30), (65, 60, 60))
+BrownBlack  =   ((0, 10, 5), (40, 40, 25))
+Green       =   ((60, 21, 50), (165, 100, 85))
+GreenGray   =   ((60, 2, 25), (165, 20, 65))
 
-    cv2.circle(image, lcenter, 10, (0, 255, 0), 1)
-    cv2.circle(image, rcenter, 10, (0, 255, 0), 1)
+def eye_color(image1, lcenter, rcenter):
+    imgHSV = cv2.cvtColor(image1, cv2.COLOR_BGR2HSV)
+    imgMask = np.zeros((image1.shape[0], image1.shape[1], 1))
+    cv2.circle(imgMask, lcenter, 25, (255,255,255), -1)
+    cv2.circle(imgMask, rcenter, 25, (255,255,255), -1)
+
+    cv2.imwrite("mask.jpg", imgMask)
+
+    h, s, v = cv2.split(imgHSV)
+
+    cv2.circle(image, lcenter, 25, (0, 255, 0), 1)
+    cv2.circle(image, rcenter, 25, (0, 255, 0), 1)
     cv2.imshow("img", image)
     #cv2.waitKey(0)
-    cv2.imwrite("test.jpg", imgHSV)
+    cv2.imwrite("test.jpg", image)
+
+
 
 if __name__ == '__main__':
     # read image 
