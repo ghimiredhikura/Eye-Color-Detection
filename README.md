7a530317ac25a4a0fd75ec7186606fcc7866c38b
Deepak Ghimire <ghmdeepak@gmail.com>
2019-08-14 17:49:03 +0900

    cleanup

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 ## Eye Color Detection
+    
+#### Usage 
+    $ python eye-color.py --image_path=data/1.jpg
 
-![Alt text](run_command.png)
+    Output: 
 
-### References
+    **** Eyes Color Percentage ****
+    Blue        :  0.0 %
+    Blue Gray   :  0.0 %
+    Brown       :  99.35 %
+    Brown Gray  :  0.22 %
+    Brown Black :  0.0 %
+    Green       :  0.33 %
+    Green Gray  :  0.11 %
+
+#### References
 1. [What color are your eyes exactly?](https://www.edow.com/general-eye-care/eyecolor/)
 2. [HTML color picker](https://www.w3schools.com/colors/colors_picker.asp?colorhex=ffff00)
 3. [What is the HSV color model?](https://www.lifewire.com/what-is-hsv-in-design-1078068)
--- eye-color.py
+++ eye-color.py
@@ -4,8 +4,8 @@ import sys
 import os
 import numpy as np
 import cv2
-from scipy.spatial import distance as dist
 import argparse
+import time
 
 # img1 l(200, 220) | r(305, 213)
 # img2 l(295, 440) | r(595, 420)
@@ -19,7 +19,7 @@ parser.add_argument('--rx', type=int, default=595)
 parser.add_argument('--ry', type=int, default=420)
 args = parser.parse_args()
 
-# define HSV color ranges for eyes color
+# define HSV color ranges for eyes colors
 Blue        =   ((166, 21, 50), (240, 100, 85))
 BlueGray    =   ((166, 2, 25), (300, 20, 75))
 Brown       =   ((2, 20, 20), (40, 100, 60))
@@ -30,39 +30,40 @@ GreenGray   =   ((60, 2, 25), (165, 20, 65))
 
 class_name = ("Blue       ", "Blue Gray  ", "Brown      ", "Brown Gray ", "Brown Black", "Green      ", "Green Gray ", "Other")
 
+def check_color(hsv, color):
+    if (hsv[0] >= color[0][0]) and (hsv[0] <= color[1][0]) and (hsv[1] >= color[0][1]) and hsv[1] <= color[1][1] and (hsv[2] >= color[0][2]) and (hsv[2] <= color[1][2]):
+        return True
+    else:
+        return False
+
 # define eye color category rules in HSV space
 def find_class(hsv):
-    if (hsv[0] >= Blue[0][0]) and (hsv[0] <= Blue[1][0]) and (hsv[1] >= Blue[0][1]) and hsv[1] <= Blue[1][1] and (hsv[2] >= Blue[0][2]) and (hsv[2] <= Blue[1][2]):
+    if check_color(hsv, Blue):
         return 0
-    elif (hsv[0] >= BlueGray[0][0]) and (hsv[0] <= BlueGray[1][0]) and (hsv[1] >= BlueGray[0][1]) and hsv[1] <= BlueGray[1][1] and (hsv[2] >= BlueGray[0][2]) and (hsv[2] <= BlueGray[1][2]):
+    elif check_color(hsv, BlueGray):
         return 1
-    elif (hsv[0] >= Brown[0][0]) and (hsv[0] <= Brown[1][0]) and (hsv[1] >= Brown[0][1]) and hsv[1] <= Brown[1][1] and (hsv[2] >= Brown[0][2]) and (hsv[2] <= Brown[1][2]):
+    elif check_color(hsv, Brown):
         return 2
-    elif (hsv[0] >= BrownGray[0][0]) and (hsv[0] <= BrownGray[1][0]) and (hsv[1] >= BrownGray[0][1]) and hsv[1] <= BrownGray[1][1] and (hsv[2] >= BrownGray[0][2]) and (hsv[2] <= BrownGray[1][2]):    
+    elif check_color(hsv, BrownGray):
         return 3
-    elif (hsv[0] >= BrownBlack[0][0]) and (hsv[0] <= BrownBlack[1][0]) and (hsv[1] >= BrownBlack[0][1]) and hsv[1] <= BrownBlack[1][1] and (hsv[2] >= BrownBlack[0][2]) and (hsv[2] <= BrownBlack[1][2]):
+    elif check_color(hsv, BrownBlack):
         return 4
-    elif (hsv[0] >= Green[0][0]) and (hsv[0] <= Green[1][0]) and (hsv[1] >= Green[0][1]) and hsv[1] <= Green[1][1] and (hsv[2] >= Green[0][2]) and (hsv[2] <= Green[1][2]):
+    elif check_color(hsv, Green):
         return 5
-    elif (hsv[0] >= GreenGray[0][0]) and (hsv[0] <= GreenGray[1][0]) and (hsv[1] >= GreenGray[0][1]) and hsv[1] <= GreenGray[1][1] and (hsv[2] >= GreenGray[0][2]) and (hsv[2] <= GreenGray[1][2]):
+    elif check_color(hsv, GreenGray):
         return 6
     else:
         return 7
 
-def eye_color(image, lcenter, rcenter):
+def eye_color(image, lc, rc):
     imgHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     imgMask = np.zeros((image.shape[0], image.shape[1], 1))
     
-    eye_radius = dist.euclidean(lcenter, rcenter)
+    eye_radius = np.linalg.norm(np.array(lc)-np.array(rc))
     eye_radius = eye_radius/15
-
-    #cv2.circle(image, lcenter, int(eye_radius), (255,0,0), 2)
-    #cv2.circle(image, rcenter, int(eye_radius), (255,0,0), 2)
-    #cv2.imwrite("eye_roi.jpg", image)
    
-    cv2.circle(imgMask, lcenter, int(eye_radius), (255,255,255), -1)
-    cv2.circle(imgMask, rcenter, int(eye_radius), (255,255,255), -1)
-    #cv2.imwrite("eye_roi_mask.jpg", imgMask)
+    cv2.circle(imgMask, lc, int(eye_radius), (255,255,255), -1)
+    cv2.circle(imgMask, rc, int(eye_radius), (255,255,255), -1)
 
     h = image.shape[0]
     w = image.shape[1]
@@ -83,9 +84,9 @@ def eye_color(image, lcenter, rcenter):
     for i in range(7):
         total_vote = total_vote+eye_class[i]
 
-    print "\n **** Eyes Color Percentage **** \n"
+    print("\n **** Eyes Color Percentage **** \n")
     for i in range(7):
-        print class_name[i], ": ", round(float((eye_class[i])/float(total_vote))*100,2), "%"
+        print(class_name[i], ": ", round(float((eye_class[i])/float(total_vote))*100,2), "%")
 
 if __name__ == '__main__':
     # read image 
